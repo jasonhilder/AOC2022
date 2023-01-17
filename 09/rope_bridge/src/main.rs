ca66e1825697a601d9f95fc4b7e32ef49107f766
jason hilder <jhilder95@gmail.com>
2023-01-17 21:56:54 +0200

    I feel its close...

--- 09/rope_bridge/src/main.rs
+++ 09/rope_bridge/src/main.rs
@@ -3,15 +3,15 @@ use std::io::{self, prelude::*, BufReader, Lines};
 
 #[derive(Debug)]
 pub struct Cell {
-    x: usize,
-    y: usize,
+    _x: usize,
+    _y: usize,
     passed: bool,
-}
 
+}
 #[derive(Debug)]
 pub struct Grid {
-    _width: usize,
-    _height: usize,
+    width: usize,
+    height: usize,
     head_pos: (usize, usize),
     tail_pos: (usize, usize),
     items: Vec<Cell>,
@@ -23,20 +23,32 @@ impl Grid {
         for y in 0..=size {
             for x in 0..=size {
                 v.push(Cell {
-                    x,
-                    y,
+                    _x: x,
+                    _y: y,
                     passed: false,
                 })
             }
         }
 
         Self {
-            _width: size,
-            _height: size,
+            width: size,
+            height: size,
             head_pos: (size, 0),
             tail_pos: (size, 0),
             items: v,
         }
+    }
+    /*
+        index = width * row + col
+        col = index % width
+        row = index / width
+    */
+    fn flat_index(&self, pos: (usize, usize)) -> usize {
+        pos.0 * (self.width - 1) + pos.1 * (self.height - 1)
+    }
+
+    pub fn at_pos(&mut self, pos: (usize, usize)) -> &Cell {
+        &self.items[self.flat_index(pos)]
     }
 
     pub fn get_passed_cells(&self) -> i32 {
@@ -50,7 +62,7 @@ impl Grid {
         count
     }
 
-    pub fn simulate_instructions(&self, instructions: Vec<(String, usize)>) {
+    pub fn simulate_instructions(&mut self, instructions: Vec<(String, usize)>) {
         /*
             Given direction, steps(num)
             track current position and move on the grid, only check passed if
@@ -59,32 +71,73 @@ impl Grid {
         for instruction in instructions {
             match instruction.0.as_str() {
                 "U" => {
-                    println!("found up instruction");
-                    println!("{}", instruction.1);
+                    self.step_grid("col", "decrease", instruction.1);
                 }
                 "D" => {
-                    println!("found down instruction");
-                    println!("{}", instruction.1);
+                    self.step_grid("col", "increase", instruction.1);
                 }
                 "L" => {
-                    println!("found left instruction");
-                    println!("{}", instruction.1);
+                    self.step_grid("row", "decrease", instruction.1);
                 }
                 "R" => {
-                    println!("found right instruction");
-                    println!("{}", instruction.1);
+                    self.step_grid("row", "increase", instruction.1);
                 }
                 _ => {}
             }
         }
     }
 
-    fn step_grid() {
+    // compare distance from head to tail, if greater than 1 between
+    // move tail one place and update its cell.
+    fn update_tail(&mut self) {
+        // mark this cell as "passed" before anything as the tail is here.
+        let tail_cell_index = self.flat_index(self.tail_pos);
+
+        if !self.items[tail_cell_index].passed {
+            self.items[tail_cell_index].passed  = true;
+        };
+
+        // compare rows first
+        if self.head_pos.0 - self.tail_pos.0 == 2 {
+            self.tail_pos = ((self.head_pos.0 - 1), self.tail_pos.1);
+        };
+
+        // compare cols
+        if self.head_pos.1 - self.tail_pos.1 == 2 {
+            self.tail_pos = (self.tail_pos.0, (self.head_pos.1 - 1));
+        };
+    }
+
+    fn step_grid(&mut self, axis: &str, direction: &str, steps: usize) {
         /*
         if steps == 1 move the head but dont update the tail,
         if steps > 1 move the head and tail, when the tail updates
         update the cell
+
+        todo:
+            using the head_pos increment along each step and set the passed field if needed
         */
+        for _s in 0..steps {
+            if axis == "row" {
+                if direction == "increase" {
+                    self.head_pos = ((self.head_pos.0 + 1), self.head_pos.1)                
+                } else {
+                    self.head_pos = ((self.head_pos.0 - 1), self.head_pos.1)                
+                }
+            } else {
+                if direction == "increase" {
+                    self.head_pos = (self.head_pos.0, (self.head_pos.1 + 1))                
+                } else {
+                    if self.head_pos.1 > 0 {
+                        self.head_pos = (self.head_pos.0, (self.head_pos.1 - 1))
+                    } else {
+                        self.head_pos = (self.head_pos.0, 0)
+                    }
+                }
+            } 
+
+            self.update_tail();
+        }
     }
 }
 
@@ -93,14 +146,16 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
     let lines = reader.lines();
     let grid_info = parse_input(lines);
-    // grid_info.0 is the max value from instructions
-    let grid = Grid::new(grid_info.0);
-    // grid_info.1 is a Vec of tuples for movements direction, steps
+
+    // grid_info.0 is the max value from instructions so we can create the square grid
+    let mut grid = Grid::new(grid_info.0);
+    // grid_info.1 is a Vec of tuples for movements == direction, steps
+    //println!("pi: {:#?}", &grid_info.1);
+
     grid.simulate_instructions(grid_info.1);
 
-    // println!("gs: {:#?}", grid);
-    // println!("pc: {}", grid.get_passed_cells());
-    // println!("pi: {:#?}", grid_info.1);
+    //println!("gs: {:#?}", grid);
+    println!("pc: {}", grid.get_passed_cells());
     Ok(())
 }
 
